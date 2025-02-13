327f284a715eb771c358cccfe05b410160ee9ef0
Gustavo Müller <113986559+gustavormuller@users.noreply.github.com>
2025-02-13 13:18:31 -0300

    exercicio 4.4

--- Exercicios4/Program.cs
+++ Exercicios4/Program.cs
@@ -12,13 +12,20 @@ namespace Exercicios4
 
             for (int i = 0; i < N; i++)
             {
-                Console.WriteLine("Digite 3 números com uma casa decimal: ");
+                Console.WriteLine("Digite pares de números: ");
                 double n1 = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
                 double n2 = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
-                double n3 = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
 
-                double media = (n1 * 2.0 + n2 * 3.0 + n3 * 5.0) / 10.0;
-                Console.WriteLine(media.ToString("F1", CultureInfo.InvariantCulture));
+                if (n2 == 0)
+                {
+                    Console.WriteLine("Divisão impossível");
+                }
+                else
+                {
+                    double divisao = n1 / n2;
+                    Console.WriteLine(divisao.ToString("F2", CultureInfo.InvariantCulture));
+                }
+
             }
         }
     }

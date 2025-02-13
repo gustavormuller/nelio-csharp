82d346eb356280cbd6c778d08e69076a02099224
Gustavo Müller <113986559+gustavormuller@users.noreply.github.com>
2025-02-13 12:40:42 -0300

    exercicio 4.1

---
+++ Exercicios4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercicios4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um valor entre 1 e 1000");
+            int x = int.Parse(Console.ReadLine()!);
+
+            for (int i = 1; i <= x; i++) {
+                if (i == 1 || i % 2 != 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}

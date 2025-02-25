67745a1c60046d8bcf8d5bcd4d78e704e2335479
Gustavo Müller <113986559+gustavormuller@users.noreply.github.com>
2025-02-25 14:42:50 -0300

    Exercicio resolvido herança

---
+++ ResolvidoHeranca/Entities/OutsourcedEmployee.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace ResolvidoHeranca.Entities
+{
+    class OutsourcedEmployee : Employee
+    {
+        public double AdditionalCharge { get; set; }
+
+        public OutsourcedEmployee()
+        {
+        }
+
+        public OutsourcedEmployee(string name, int hours, double valuePerHour, double additionalCharge) : base(name, hours, valuePerHour)
+        {
+            AdditionalCharge = additionalCharge;
+        }
+
+        public override double Payment()
+        {
+            return base.Payment() + 1.10 * AdditionalCharge;
+        }
+    }
+}

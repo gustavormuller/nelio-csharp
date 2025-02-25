2db771844027e95282881f580ccab2409af62054
Gustavo Müller <113986559+gustavormuller@users.noreply.github.com>
2025-02-25 16:27:16 -0300

    exercicio fixacao herança

---
+++ FixacaoHeranca/Entities/Product.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Globalization;
+
+namespace FixacaoHeranca.Entities
+{
+    class Product
+    {
+        public string? Name { get; set; }
+        public double Price { get; set; }
+
+        public Product()
+        {
+        }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public virtual string PriceTag()
+        {
+            return Name
+                + " $ "
+                + Price.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}

8a5239d413101921301a343ede4ef6835c095912
Gustavo Müller <113986559+gustavormuller@users.noreply.github.com>
2025-03-07 16:27:38 -0300

    exercicio proposto dictionary

---
+++ PropostoDictionary/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace PropostoDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter file full path: ");
+            string path = Console.ReadLine();
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+
+                    Dictionary<string, int> dictionary = new();
+
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(',');
+                        string name = line[0];
+                        int votos = int.Parse(line[1]);
+
+                        if (dictionary.ContainsKey(name))
+                        {
+                            dictionary[name] += votos;
+                        }
+                        else
+                        {
+                            dictionary[name] = votos;
+                        }
+                    }
+
+                    foreach (var item in dictionary)
+                    {
+                        Console.WriteLine(item.Key + " - " + item.Value);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}

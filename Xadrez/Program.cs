daf66906ccc6f017b0c325401667fc0f2f5ab98e
Gustavo Müller <113986559+gustavormuller@users.noreply.github.com>
2025-03-17 16:20:25 -0300

    implementa classe tabuleiro

---
+++ Xadrez/Program.cs
@@ -0,0 +1,14 @@
+﻿using tabuleiro;
+
+namespace Xadrez
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tabuleiro tab = new Tabuleiro(8, 8);
+
+            Console.ReadLine();
+        }
+    }
+}

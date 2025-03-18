2a332c6b1500b574f7d9fa103e188e841aa0d10d
Gustavo Müller <113986559+gustavormuller@users.noreply.github.com>
2025-03-18 14:24:44 -0300

    melhora impressão do tabuleiro

--- Xadrez/Program.cs
+++ Xadrez/Program.cs
@@ -12,8 +12,10 @@ namespace Xadrez
                 Tabuleiro tab = new Tabuleiro(8, 8);
 
                 tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 9));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
                 tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
+
+                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(3, 5));
 
                 Tela.imprimirTabuleiro(tab);
             }
